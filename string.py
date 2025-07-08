d3bc50376ff533e06688f1269fb0b61f892cd06f
jagannadhduddukuru <jagannadhduddukuru35@gmail.com>
2025-07-08 11:54:39 +0530

    starting

---
+++ string.py
@@ -0,0 +1,44 @@
+#name = "jagannadh"
+#print(name[0])
+
+#name = "jagannadh"
+#print(name[len(name)-1])
+
+#name = "jagannadh"
+#surname = "duddukuru"
+#full_name = name + surname
+#print(full_name)
+
+#name = "jagannadh"
+#for i in range (3):
+#    print(name)
+
+#name = "jagannadh"
+#print(name[0:-4])
+
+#name = "jagannadh"
+#print(name[::-1])
+
+#name =  "main duddukuru jagannadh"
+#sub_name = "duddukuru"
+#for i in range(len(name)):
+ #   past=name[i:len(sub_name)]
+  #  if past == sub_name:
+   #    print(sub_name,"is a part of",name)
+    #   break
+
+#name = "jagannadh"
+#print(len(name))
+
+#name="jagannadh"
+#print(name.upper())
+
+#sentance = "listing to the git hub class"
+#print(sentance.replace(" ","__"))
+
+#name = "jagannadh"
+#print(name.find("h"))
+
+
+name = ['playing', 'a', 'cricket']
+print(" ".join(name))
